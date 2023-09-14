4b034dcea4c8aa5eb7c76626a372c29e741528aa
Richy <garrincha33@users.noreply.github.com>
2023-09-14 13:20:14 +0100

    Initial Commit

---
+++ SwiftUIThingsILike/SwiftUIThingsILikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThingsILikeApp.swift
+//  SwiftUIThingsILike
+//
+//  Created by Richard Price on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThingsILikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
